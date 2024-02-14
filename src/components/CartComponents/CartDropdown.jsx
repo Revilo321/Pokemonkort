eea6874f91562a2c4ef2c983db85f9efa033dccc
Revilo321 <58446652+Revilo321@users.noreply.github.com>
2024-02-14 14:50:41 +0100

    edited link to detailspage

--- src/components/CartComponents/CartDropdown.jsx
+++ src/components/CartComponents/CartDropdown.jsx
@@ -1,18 +1,18 @@
-import { useSelector } from 'react-redux'
-import { CartItem } from './CarItem'
+import { useSelector } from "react-redux";
+import { CartItem } from "./CarItem";
 
 export const CartDropdown = () => {
-  const cartItems = useSelector((state) => state.cart.items)
+  const cartItems = useSelector((state) => state.cart.items);
 
   return (
-    <div className='absolute right-0 w-48 bg-white text-black shadow-lg mt-2'>
+    <div className="absolute -right-5 top-9 w-48 bg-white text-black shadow-lg mt-2">
       {Object.entries(cartItems).length === 0 ? (
-        <div className='p-4'>Your cart is empty.</div>
+        <div className="p-4">Your cart is empty.</div>
       ) : (
         Object.entries(cartItems).map(([id, item]) => (
           <CartItem key={id} item={item} />
         ))
       )}
     </div>
-  )
-}
+  );
+};
--- src/components/CocktailComponents/CocktailCard.jsx
+++ src/components/CocktailComponents/CocktailCard.jsx
@@ -10,40 +10,38 @@ const CocktailCard = ({ cocktail }) => {
     dispatch(addToCart(cocktail));
   };
   return (
-    <Link to={`/cocktail/${cocktail.idDrink}`}>
-      <div className="max-w-sm rounded overflow-hidden h-full shadow-lg relative">
-        <img
-          className="w-full"
-          src={cocktail.strDrinkThumb}
-          alt={cocktail.strDrink}
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{cocktail.strDrink}</div>
-          <p className="text-gray-700 text-base">
-            {cocktail.strInstructions
-              ? cocktail.strInstructions.substring(0, 100) + "..."
-              : ""}
-          </p>
-        </div>
-        <div className="px-6 pt-4 pb-12">
-          {cocktail.strTags?.split(",").map((tag) => (
-            <span
-              key={tag}
-              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >
-              #{tag}
-            </span>
-          ))}
-        </div>
-        {/* Cocktail image and details */}
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute bottom-2 left-5"
-          onClick={handleAddToCart}
-        >
-          Add to Cart
-        </button>
+    <div className="max-w-sm rounded overflow-hidden h-full shadow-lg relative">
+      <img
+        className="w-full"
+        src={cocktail.strDrinkThumb}
+        alt={cocktail.strDrink}
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{cocktail.strDrink}</div>
+        <p className="text-gray-700 text-base">
+          {cocktail.strInstructions
+            ? cocktail.strInstructions.substring(0, 100) + "..."
+            : ""}
+        </p>
       </div>
-    </Link>
+      <div className="px-6 pt-4 pb-12">
+        {cocktail.strTags?.split(",").map((tag) => (
+          <span
+            key={tag}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >
+            #{tag}
+          </span>
+        ))}
+      </div>
+      {/* Cocktail image and details */}
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute bottom-2 left-5"
+        onClick={handleAddToCart}
+      >
+        Add to Cart
+      </button>
+    </div>
   );
 };
 
